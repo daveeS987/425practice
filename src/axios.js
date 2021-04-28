bd102c154192256e1bbd4146532b20d790f11d7b
Davee Sok <daveesok@gmail.com>
2021-04-27 20:33:47 -0500

    cleaned up schema file of console log

--- src/axios.js
+++ src/axios.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
-let testing = process.env.REACT_APP_BACKEND;
-console.log('🚀 ~ file: axios.js ~ line 4 ~ testing', testing);
-
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND}`,
 });
