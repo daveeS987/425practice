64dba0916772a9d0241b283bcffe49a9d65730eb
Davee Sok <daveesok@gmail.com>
2021-04-27 23:14:29 -0500

    testing to see if this will deploy

--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,8 @@
 import axios from 'axios';
+// const functions = require('firebase-functions');
+
+// let testEnv = functions.config().reactbackend.url;
+// console.log('🚀 ~ file: axios.js ~ line 5 ~ testEnv', testEnv);
 
 const instance = axios.create({
   baseURL: `${process.env.REACT_APP_BACKEND}`,
